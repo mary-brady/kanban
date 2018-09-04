d9f6b0a1168dc81aa3afd7f7bcd8df2f31a6d2ef
Mary Brady <emceebrady@gmail.com>
2018-09-04 14:23:47 -0600

    session secret & db url

--- server/server-assets/auth/session.js
+++ server/server-assets/auth/session.js
@@ -2,7 +2,7 @@ var expressSession = require("express-session");
 var mongoStore = require("connect-mongodb-session")(expressSession);
 
 var store = new mongoStore({
-  uri: "mongodb://student:student@ds151207.mlab.com:51207/bcw-junk", //CHANGE ME!!!!!!
+  uri: "mongodb://admin:admin123@ds020208.mlab.com:20208/kanban",
   collection: "Sessions"
 });
 
@@ -12,7 +12,7 @@ store.on("error", function (err) {
 
 // @ts-ignore
 var session = expressSession({
-  secret: "you should change this", //CHANGE ME!!!!
+  secret: "s3cr3t th1ng we n33ded",
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 7 * 52 * 2,
   },
