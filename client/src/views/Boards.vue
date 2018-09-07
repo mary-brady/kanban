b05128f5cd6591ec328cc6753116da45e78e8f0c
Mary Brady <emceebrady@gmail.com>
2018-09-07 15:36:08 -0600

    some styling

--- client/src/views/Boards.vue
+++ client/src/views/Boards.vue
@@ -1,74 +1,88 @@
 <template>
-  <div class="boards">
-    WELCOME TO THE BOARDS!!!
+  <div class="boards container-fluid">
+    <div class="row">
+      <div class="col-md-12 justify-content-center">
+    <h1>Boards on Boards!</h1>
+    </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12 justify-content-center">
     <form @submit.prevent="addBoard">
       <input type="text" placeholder="title" v-model="newBoard.title" required>
       <input type="text" placeholder="description" v-model="newBoard.description">
       <button type="submit">Create Board</button>
     </form>
     <button @click="logout">Logout</button>
-    <div v-for="board in boards" :key="board._id">
-      <router-link :to="{name: 'board', params: {boardId: board._id}}">{{board.title}}</router-link>
+    </div>
+    </div>
+    <div class="row justify-content-center">
+    <div v-for="board in boards" :key="board._id" class="card border-primary mb-3 col-md-3 ml-3 mt-3" style="max-width: 15rem;">
+      <div class="card-header"><router-link :to="{name: 'board', params: {boardId: board._id}}"><strong>{{board.title}}</strong></router-link>
+      </div>
+          <div class="card-body">
+      <p class="card-text">{{board.description}}</p>
       <button @click="deleteBoard(board._id)">Delete Board</button>
+    </div>
+    </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "boards",
-    created() {
-      //blocks users not logged in
-      if (!this.$store.state.user._id) {
-        this.$router.push({ name: "login" });
-      }
-    },
-    mounted() {
-      this.$store.dispatch("getBoards");
-    },
-    data() {
-      return {
-        newBoard: {
-          title: "",
-          description: ""
-        }
-      };
-    },
-    computed: {
-      boards() {
-        return this.$store.state.boards
-      },
-
-      lists() {
-        return this.$store.state.lists
-      },
-
-      tasks() {
-        return this.$store.state.tasks
-      }
-
-    },
-    methods: {
-
-      deleteBoard(boardId) {
-        this.lists[boardId].forEach(list => {
-          this.tasks[list._id].forEach(task => {
-            this.$store.dispatch('deleteTaskComments', task._id)
-          })
-          this.$store.dispatch('deleteListTasks', list._id)
-        })
-        this.$store.dispatch('deleteBoardLists', boardId)
-        this.$store.dispatch("deleteBoard", boardId)
-      },
-
-      addBoard() {
-        this.$store.dispatch("addBoard", this.newBoard);
-        this.newBoard = { title: "", description: "" };
-      },
-
-      logout() {
-        this.$store.dispatch('logout')
-      }
+export default {
+  name: "boards",
+  created() {
+    //blocks users not logged in
+    if (!this.$store.state.user._id) {
+      this.$router.push({ name: "login" });
     }
-  };
+  },
+  mounted() {
+    this.$store.dispatch("getBoards");
+  },
+  data() {
+    return {
+      newBoard: {
+        title: "",
+        description: ""
+      }
+    };
+  },
+  computed: {
+    boards() {
+      return this.$store.state.boards;
+    },
+
+    lists() {
+      return this.$store.state.lists;
+    },
+
+    tasks() {
+      return this.$store.state.tasks;
+    }
+  },
+  methods: {
+    deleteBoard(boardId) {
+      this.lists[boardId].forEach(list => {
+        this.tasks[list._id].forEach(task => {
+          this.$store.dispatch("deleteTaskComments", task._id);
+        });
+        this.$store.dispatch("deleteListTasks", list._id);
+      });
+      this.$store.dispatch("deleteBoardLists", boardId);
+      this.$store.dispatch("deleteBoard", boardId);
+    },
+
+    addBoard() {
+      this.$store.dispatch("addBoard", this.newBoard);
+      this.newBoard = { title: "", description: "" };
+    },
+
+    logout() {
+      this.$store.dispatch("logout");
+    }
+  }
+};
 </script>
+<style scoped>
+</style>
